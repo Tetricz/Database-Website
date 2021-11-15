b8693d35420f819801c24240387085fbade54ac7
Ariyune <dannyphan441@gmail.com>
2021-11-15 13:50:26 -0600

    Changed job.sql and tables (updated with country names)

--- table_sql/job.sql
+++ table_sql/job.sql
@@ -1,4 +1,14 @@
-create table employee (
-	social_security_num VARCHAR(50),
-    job VARCHAR(50)
+create table job (
+    job VARCHAR(20),
+    payrate DECIMAL (3, 2),
+    overtime_payrate DECIMAL (3, 2),
+    benefits VARCHAR(20),
+    sick_leave INT,
+    vacation_days INT
 );
+
+insert into job (job, payrate, overtime_payrate, benefits, sick_leave, vacation_days) values ('pilot', '', '', 'medical, retirement, travel expenses', 9, 16);
+insert into job (job, payrate, overtime_payrate, benefits, sick_leave, vacation_days) values ('copilot', '', '', 'medical, retirement, travel expenses', 9, 16);
+insert into job (job, payrate, overtime_payrate, benefits, sick_leave, vacation_days) values ('flight attendant', '', '', 'medical, retirement, travel expenses', 9, 14);
+insert into job (job, payrate, overtime_payrate, benefits, sick_leave, vacation_days) values ('ground crew', '', '', 'medical, retirement, workers compensation', 9, 14);
+insert into job (job, payrate, overtime_payrate, benefits, sick_leave, vacation_days) values ('office worker', '', '', 'medical, retirement, stock investments', 9, 14);
