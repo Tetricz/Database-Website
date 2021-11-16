8eb597f2daa8b53018ad930d8a6b07e22cbe170f
Tetricz <DavidLeeDaniels@protonmail.com>
2021-11-15 20:06:09 -0600

    added airport name

--- table_sql/airport.sql
+++ table_sql/airport.sql
@@ -4,8 +4,8 @@ create table airport (
     airport_name VARCHAR(50)
 );
 
-insert into airport (airport_code, city) values ('PEK', 'Beijing', 'Beijing Capital International Airport');
-insert into airport (airport_code, city) values ('SYD', 'Sydney', 'Sydney Airport');
-insert into airport (airport_code, city) values ('LAX', 'Los Angeles', "Los Angeles International Airport");
-insert into airport (airport_code, city) values ('CDG', 'Paris', 'Charles de Gaulle Airport');
-insert into airport (airport_code, city) values ('GRU', 'Sao Paulo', 'Guarulhos International Airport');
+insert into airport (airport_code, city, airport_name) values ('PEK', 'Beijing', 'Beijing Capital International Airport');
+insert into airport (airport_code, city, airport_name) values ('SYD', 'Sydney', 'Sydney Airport');
+insert into airport (airport_code, city, airport_name) values ('LAX', 'Los Angeles', "Los Angeles International Airport");
+insert into airport (airport_code, city, airport_name) values ('CDG', 'Paris', 'Charles de Gaulle Airport');
+insert into airport (airport_code, city, airport_name) values ('GRU', 'Sao Paulo', 'Guarulhos International Airport');
