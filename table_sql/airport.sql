af0d1a5eff8677f5055278ad88dbdc21e5319a87
Ariyune <dannyphan441@gmail.com>
2021-11-16 14:28:20 -0600

    Fixed formatting errors for sql (uploaded to db)

--- table_sql/airport.sql
+++ table_sql/airport.sql
@@ -6,6 +6,6 @@ create table airport (
 
 insert into airport (airport_code, city, airport_name) values ('PEK', 'Beijing', 'Beijing Capital International Airport');
 insert into airport (airport_code, city, airport_name) values ('SYD', 'Sydney', 'Sydney Airport');
-insert into airport (airport_code, city, airport_name) values ('LAX', 'Los Angeles', "Los Angeles International Airport");
+insert into airport (airport_code, city, airport_name) values ('LAX', 'Los Angeles', 'Los Angeles International Airport');
 insert into airport (airport_code, city, airport_name) values ('CDG', 'Paris', 'Charles de Gaulle Airport');
 insert into airport (airport_code, city, airport_name) values ('GRU', 'Sao Paulo', 'Guarulhos International Airport');
