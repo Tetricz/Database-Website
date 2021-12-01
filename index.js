1430709754746e5e8bf9357258eb7056f10acd04
Ariyune <dannyphan441@gmail.com>
2021-11-30 19:03:51 -0600

    Commented select statements, benefits to employee

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get('/flightassignments', async(req, res)=>{
   try{
     sqlquery = 'SELECT * FROM flightassignment;'
     const allDemos = await pool.query(sqlquery);
-    await appendtofile(sql, sqlquery)
+    appendtofile(sql, sqlquery)
     res.json(allDemos.rows);
   } catch(err){
     console.log(err.message);
@@ -32,7 +32,7 @@ app.get('/officeshifts', async(req, res)=>{
   try{
     sqlquery = 'SELECT * FROM officeshift;'
     const allDemos = await pool.query(sqlquery);
-    await appendtofile(sql, sqlquery)
+    appendtofile(sql, sqlquery)
     res.json(allDemos.rows);
   } catch(err){
     console.log(err.message);
@@ -43,11 +43,12 @@ app.get('/officeshifts', async(req, res)=>{
 app.get('/employee_info', async(req, res)=>{
   try{
     sqlquery = 
-`SELECT *
-FROM employee 
+`SELECT social_security_num, first_name, last_name, email, gender, street_num, city, country, employee.job,
+current_airport_code, medical_benefits, retirement_benefits, travel_expenses, workers_compensation FROM employee, job 
+WHERE employee.job = job.job 
 ORDER BY first_name, last_name;`;
     const allDemos = await pool.query(sqlquery);
-    await appendtofile(sql, sqlquery)
+    appendtofile(sql, `/*Selects ssn, first and last name, email, gender, street num, city, country, job, current airport code, and benefits from the employee and job tables-->*/\n` + sqlquery)
     res.json(allDemos.rows);
   } catch(err){
     console.log(err.message);
@@ -136,7 +137,11 @@ FROM (
       ON t1.social_security_num = t2.social_security_num
 ) AS t3;`
     const allDemos = await pool.query(sqlquery);
-    await appendtofile(sql, sqlquery)
+    appendtofile(sql, `/*Selects ssn, hours worked, overtime hours worked, normal payrate, overtime payrate, taxes and
+    monthly salary from employee, flightassignmens, job and country tables.
+    We find all the flights/office shifts assigned to a social security number, get the total hours worked, place anything worked above 40 hours as overtime,
+    then we pull from the job table to get the payrate for the employee and calculate their monthly salary by multiplying hours worked by the appropriate
+    payrate. Lastly we get the tax for the employee's country and multiply it by the previous result, and subtract it to get the monthly salary-->*/\n` + sqlquery)
     res.json(allDemos.rows);
     await paymentupdate(allDemos.rows);
   } catch(err){
@@ -154,7 +159,7 @@ app.get('/flightassignments/:departure_airport/:job', async(req, res)=>{
 FROM employee 
 WHERE current_airport_code = '${departure_airport}' AND job = '${job}';`
     const demo = await pool.query(sqlquery);
-    await appendtofile(sql, sqlquery)
+    appendtofile(sql, `/*Selects ssns to assign that have their current_airport_code the same as the flight departure airport code: ${departure_airport} and job: ${job}-->*/\n` + sqlquery)
     res.json(demo.rows);
   } catch(err){
     console.log(err.message);
@@ -173,7 +178,7 @@ WHERE current_airport_code =
     WHERE office_num = ${office_id}) 
     AND job = '${job}';`
     const demo = await pool.query(sqlquery);
-    await appendtofile(sql, sqlquery)
+    appendtofile(sql, `/*Selects ssns to assign that have their current_airport_code the same as the office id: ${office_id} airport code and job: ${job}-->*/\n` + sqlquery)
     res.json(demo.rows);
   } catch(err){
     console.log(err.message);
@@ -194,7 +199,7 @@ WHERE flight_id LIKE '${id}';`
     await pool.query(transactionquery);
     await pool.query('COMMIT')
     transactionquery = 'BEGIN;\n' + transactionquery + '\nCOMMIT;'
-    await appendtofile(transactions, transactionquery)
+    appendtofile(transactions, transactionquery)
     res.json({pilot, copilot, flight_attendant_1, flight_attendant_2, 
       flight_attendant_3, flight_attendant_4})
   } catch (err) {
@@ -215,7 +220,7 @@ WHERE shift_id = ${id};`
     await pool.query(transactionquery);
     await pool.query('COMMIT')
     transactionquery = 'BEGIN;\n' + transactionquery + '\nCOMMIT;'
-    await appendtofile (transactions, transactionquery)
+    appendtofile (transactions, transactionquery)
     res.json({ground_worker_1, ground_worker_2, office_worker_1, office_worker_2})
   } catch (err) {
     console.error(err.message);
@@ -254,7 +259,7 @@ async function paymentupdate (payments) {
           await pool.query(transactionquery);
           await pool.query('COMMIT')
           transactionquery = 'BEGIN;\n' + transactionquery + '\nCOMMIT;'
-          await appendtofile (transactions, transactionquery)
+          appendtofile (transactions, transactionquery)
     }
   } catch (err) {
     console.error(err.message);
@@ -274,7 +279,7 @@ WHERE social_security_num = '${id}';`
     await pool.query(transactionquery);
     await pool.query('COMMIT')
     transactionquery = 'BEGIN;\n' + transactionquery + '\nCOMMIT;'
-    await appendtofile (transactions, transactionquery)
+    appendtofile (transactions, transactionquery)
     res.json({first_name, last_name, email, gender, street_address, city, country})
   } catch (err) {
     console.error(err.message);
--- transactions.sql
+++ transactions.sql
@@ -1,233 +1,25 @@
 BEGIN;
-UPDATE flightassignment SET pilot = '605-24-9827', copilot = '280-33-7851', flight_attendant_1 = '222-16-3379', flight_attendant_2 = '763-40-1461', 
-flight_attendant_3 = '347-67-1273', flight_attendant_4 = '548-68-1810'
+UPDATE flightassignment SET pilot = '500-79-5396', copilot = '545-56-1535', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
+flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
 WHERE flight_id LIKE '01_';
 COMMIT;
 BEGIN;
 Update payment
 SET normal_hours = CASE social_security_num
-                     WHEN '605-24-9827' THEN 26
-                     WHEN '280-33-7851' THEN 26
-                     WHEN '347-67-1273' THEN 26
-                     WHEN '222-16-3379' THEN 26
-                     WHEN '763-40-1461' THEN 26
-                     WHEN '548-68-1810' THEN 26
+                     WHEN '500-79-5396' THEN 26
+                     WHEN '545-56-1535' THEN 26
                    END,
     overtime_hours = CASE social_security_num
-                     WHEN '605-24-9827' THEN 0
-                     WHEN '280-33-7851' THEN 0
-                     WHEN '347-67-1273' THEN 0
-                     WHEN '222-16-3379' THEN 0
-                     WHEN '763-40-1461' THEN 0
-                     WHEN '548-68-1810' THEN 0
+                     WHEN '500-79-5396' THEN 0
+                     WHEN '545-56-1535' THEN 0
                    END,
     taxes = CASE social_security_num
-                     WHEN '605-24-9827' THEN 509.60
-                     WHEN '280-33-7851' THEN 473.20
-                     WHEN '347-67-1273' THEN 276.64
-                     WHEN '222-16-3379' THEN 276.64
-                     WHEN '763-40-1461' THEN 276.64
-                     WHEN '548-68-1810' THEN 276.64
+                     WHEN '500-79-5396' THEN 509.60
+                     WHEN '545-56-1535' THEN 473.20
                    END,
     monthly_salary = CASE social_security_num
-                     WHEN '605-24-9827' THEN 1310.40
-                     WHEN '280-33-7851' THEN 1216.80
-                     WHEN '347-67-1273' THEN 711.36
-                     WHEN '222-16-3379' THEN 711.36
-                     WHEN '763-40-1461' THEN 711.36
-                     WHEN '548-68-1810' THEN 711.36
+                     WHEN '500-79-5396' THEN 1310.40
+                     WHEN '545-56-1535' THEN 1216.80
                    END
-WHERE social_security_num IN ('605-24-9827', '280-33-7851', '347-67-1273', '222-16-3379', '763-40-1461', '548-68-1810');
-COMMIT;
-BEGIN;
-Update payment
-SET normal_hours = CASE social_security_num
-                     WHEN '605-24-9827' THEN 26
-                     WHEN '280-33-7851' THEN 26
-                     WHEN '347-67-1273' THEN 26
-                     WHEN '222-16-3379' THEN 26
-                     WHEN '763-40-1461' THEN 26
-                     WHEN '548-68-1810' THEN 26
-                   END,
-    overtime_hours = CASE social_security_num
-                     WHEN '605-24-9827' THEN 0
-                     WHEN '280-33-7851' THEN 0
-                     WHEN '347-67-1273' THEN 0
-                     WHEN '222-16-3379' THEN 0
-                     WHEN '763-40-1461' THEN 0
-                     WHEN '548-68-1810' THEN 0
-                   END,
-    taxes = CASE social_security_num
-                     WHEN '605-24-9827' THEN 509.60
-                     WHEN '280-33-7851' THEN 473.20
-                     WHEN '347-67-1273' THEN 276.64
-                     WHEN '222-16-3379' THEN 276.64
-                     WHEN '763-40-1461' THEN 276.64
-                     WHEN '548-68-1810' THEN 276.64
-                   END,
-    monthly_salary = CASE social_security_num
-                     WHEN '605-24-9827' THEN 1310.40
-                     WHEN '280-33-7851' THEN 1216.80
-                     WHEN '347-67-1273' THEN 711.36
-                     WHEN '222-16-3379' THEN 711.36
-                     WHEN '763-40-1461' THEN 711.36
-                     WHEN '548-68-1810' THEN 711.36
-                   END
-WHERE social_security_num IN ('605-24-9827', '280-33-7851', '347-67-1273', '222-16-3379', '763-40-1461', '548-68-1810');
-COMMIT;
-BEGIN;
-Update payment
-SET normal_hours = CASE social_security_num
-                     WHEN '605-24-9827' THEN 26
-                     WHEN '280-33-7851' THEN 26
-                     WHEN '347-67-1273' THEN 26
-                     WHEN '222-16-3379' THEN 26
-                     WHEN '763-40-1461' THEN 26
-                     WHEN '548-68-1810' THEN 26
-                   END,
-    overtime_hours = CASE social_security_num
-                     WHEN '605-24-9827' THEN 0
-                     WHEN '280-33-7851' THEN 0
-                     WHEN '347-67-1273' THEN 0
-                     WHEN '222-16-3379' THEN 0
-                     WHEN '763-40-1461' THEN 0
-                     WHEN '548-68-1810' THEN 0
-                   END,
-    taxes = CASE social_security_num
-                     WHEN '605-24-9827' THEN 509.60
-                     WHEN '280-33-7851' THEN 473.20
-                     WHEN '347-67-1273' THEN 276.64
-                     WHEN '222-16-3379' THEN 276.64
-                     WHEN '763-40-1461' THEN 276.64
-                     WHEN '548-68-1810' THEN 276.64
-                   END,
-    monthly_salary = CASE social_security_num
-                     WHEN '605-24-9827' THEN 1310.40
-                     WHEN '280-33-7851' THEN 1216.80
-                     WHEN '347-67-1273' THEN 711.36
-                     WHEN '222-16-3379' THEN 711.36
-                     WHEN '763-40-1461' THEN 711.36
-                     WHEN '548-68-1810' THEN 711.36
-                   END
-WHERE social_security_num IN ('605-24-9827', '280-33-7851', '347-67-1273', '222-16-3379', '763-40-1461', '548-68-1810');
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = '830-55-4393', copilot = '386-94-2181', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '02_';
-COMMIT;
-BEGIN;
-BEGIN;
-UPDATE flightassignment SET pilot = '830-55-4393', copilot = '386-94-2181', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '02_';
-COMMIT;
-COMMIT;
-BEGIN;
-BEGIN;
-BEGIN;
-UPDATE flightassignment SET pilot = '830-55-4393', copilot = '386-94-2181', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '02_';
-COMMIT;
-COMMIT;
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = '110-01-2872', copilot = '386-94-2181', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '05_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = '830-55-4393', copilot = '603-14-9498', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '02_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = 'N/A', copilot = 'N/A', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '01_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = 'N/A', copilot = 'N/A', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '02_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = 'N/A', copilot = 'N/A', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '05_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = 'N/A', copilot = 'N/A', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = '347-67-1273'
-WHERE flight_id LIKE '01_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = 'N/A', copilot = 'N/A', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '01_';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = 'Albie', last_name = 'Crocombe', email = 'acrocombe2p@linkedin.com', gender = 'Male', street_num = '79', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '360-18-7737';
-COMMIT;
-BEGIN;
-BEGIN;
-UPDATE employee
-SET first_name = 'Albie', last_name = 'Crocombe', email = 'acrocombe2p@linkedin.com', gender = 'Male', street_num = '79', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '360-18-7737';
-COMMIT;
-COMMIT;
-BEGIN;
-BEGIN;
-BEGIN;
-UPDATE employee
-SET first_name = 'Albie', last_name = 'Crocombe', email = 'acrocombe2p@linkedin.com', gender = 'Male', street_num = '79', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '360-18-7737';
-COMMIT;
-COMMIT;
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = '605-24-9827', copilot = '280-33-7851', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '01_';
-COMMIT;
-BEGIN;
-UPDATE flightassignment SET pilot = 'N/A', copilot = 'N/A', flight_attendant_1 = 'N/A', flight_attendant_2 = 'N/A', 
-flight_attendant_3 = 'N/A', flight_attendant_4 = 'N/A'
-WHERE flight_id LIKE '01_';
-COMMIT;
-BEGIN;
-UPDATE officeshift 
-SET ground_worker_1 = '158-61-5318', ground_worker_2 = '689-20-0248', office_worker_1 = '562-83-3028', office_worker_2 = '422-04-9876'
-WHERE shift_id = 1;
-COMMIT;
-BEGIN;
-Update payment
-SET normal_hours = CASE social_security_num
-                     WHEN '689-20-0248' THEN 8
-                     WHEN '158-61-5318' THEN 8
-                     WHEN '422-04-9876' THEN 8
-                     WHEN '562-83-3028' THEN 8
-                   END,
-    overtime_hours = CASE social_security_num
-                     WHEN '689-20-0248' THEN 0
-                     WHEN '158-61-5318' THEN 0
-                     WHEN '422-04-9876' THEN 0
-                     WHEN '562-83-3028' THEN 0
-                   END,
-    taxes = CASE social_security_num
-                     WHEN '689-20-0248' THEN 56.00
-                     WHEN '158-61-5318' THEN 56.00
-                     WHEN '422-04-9876' THEN 70.00
-                     WHEN '562-83-3028' THEN 70.00
-                   END,
-    monthly_salary = CASE social_security_num
-                     WHEN '689-20-0248' THEN 104.00
-                     WHEN '158-61-5318' THEN 104.00
-                     WHEN '422-04-9876' THEN 130.00
-                     WHEN '562-83-3028' THEN 130.00
-                   END
-WHERE social_security_num IN ('689-20-0248', '158-61-5318', '422-04-9876', '562-83-3028');
+WHERE social_security_num IN ('500-79-5396', '545-56-1535');
 COMMIT;
