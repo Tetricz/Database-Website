917b26f21962350152581bf8ec24fa35115f7c7e
Ariyune <dannyphan441@gmail.com>
2021-12-01 00:54:45 -0600

    Added validations, can add employee now

--- public/employee.js
+++ public/employee.js
@@ -1,6 +1,8 @@
 // create array of global variable demos
 let demos = []
 
+let newssn = ''
+
 // function to set demos
 const setDemos = (data) => {
   demos = data;
@@ -17,10 +19,103 @@ const editDemo = (id) => {
   document.querySelector('#save-edit-demo').addEventListener("click", function() {updateDemo(id)});
 }
 
+async function insertDemo (id) {
+  console.log(id)
+  var first_name = document.querySelector('#new-firstname').value
+  var last_name = document.querySelector('#new-lastname').value
+  var email = document.querySelector('#new-email').value
+  var gender = document.querySelector('#new-gender').value
+  var street_address = document.querySelector('#new-address').value
+  var city = document.querySelector('#new-city').value
+  var country = document.querySelector('#new-country').value
+  var job = document.querySelector('#new-job').value
+  var airport_code = document.querySelector('#new-airport-code').value
+  if(first_name == '' || last_name == '' || email == ''|| street_address == ''||
+      city == '' || country == '')
+  { 
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
+                                                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                            <strong>Unsuccessful!</strong> Please don't leave any box empty
+                                                          </div>`;
+  }
+  else if (!isLetterSpace.test(first_name) || !isLetterSpace.test(last_name)){
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
+                                                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                            <strong>Unsuccessful!</strong> Please enter letters or spaces for your name.
+                                                          </div>`;
+  }
+  
+  else if (!email.includes('@') && !email.includes('.')){
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
+                                                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                            <strong>Unsuccessful!</strong> Please enter a valid email address.
+                                                          </div>`;
+  }
+
+  else if (!isLetterSpace.test(first_name) || !isLetterSpace.test(last_name)){
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
+                                                            <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                            <strong>Unsuccessful!</strong> Please enter letters or spaces for your name.
+                                                          </div>`;
+  }
+  else {
+    try {
+      const body = {id: id, first_name: first_name, last_name: last_name, email: email, gender: gender,
+                      street_address: street_address, city: city, country: country, job: job, airport_code: airport_code};
+      const response = await fetch(`http://localhost:5000/insertemployee/`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(body)
+      })
+      
+      selectDemos()
+      
+      document.querySelector('#alert').innerHTML += `<div class="alert alert-success alert-dismissible">
+                                                              <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                                              <strong>Success!</strong> Please close to see the changes made.
+                                                            </div>`;
+    
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
+
+const changebenefits = () => {
+  switch (document.querySelector('#new-job').value) {
+    case "pilot" || "copilot":
+      document.querySelector('#new-benefits').innerHTML = "Medical Retirement Travel Expenses";
+      break;
+    case "flight attendant":
+      document.querySelector('#new-benefits').innerHTML = "Medical Retirement Travel Expenses";
+      break;
+    case "ground crew":
+      document.querySelector('#new-benefits').innerHTML = "Medical Retirement Workers Compensation";
+      break;
+    case "office worker":
+      document.querySelector('#new-benefits').innerHTML = "Medical Retirement Workers Compensation";
+      break;
+    default:
+      break;
+  }
+}
+
 // function to display demos
 const displayDemos = () => {
-  
   const demoTable = document.querySelector('#demo-table');
+
+  ssnexists = true;
+  while (ssnexists) {
+    newssn = Math.floor(100000000 + Math.random() * 900000000).toString()
+    newssn = `${newssn.substring(0,3)}-${newssn.substring(3,5)}-${newssn.substring(5,10)}`
+    for (let i = 0; i < demos.length; i++) {
+      if (demos[i].social_security_num === "500-79-5396"){
+        ssnexists = false;
+        break;
+      }
+    }
+  }
+  document.querySelector('#new-ssn').innerHTML = newssn;
 
   // display all demos by modifying the HTML 
   let tableHTML = "";
@@ -37,9 +132,9 @@ const displayDemos = () => {
       let benefits = ''
       demo.medical_benefits === true ?  benefits += 'Medical ' : null
       demo.retirement_benefits === true ?  benefits += 'Retirement ' : null
-      demo.travel_expenses === true ?  benefits += 'Travel expenses ' : null
-      demo.workers_compensation === true ?  benefits += 'Workers compensation ' : null
-
+      demo.travel_expenses === true ?  benefits += 'Travel Expenses ' : null
+      demo.workers_compensation === true ?  benefits += 'Workers Compensation ' : null
+      
       tableHTML +=
       `<tr Social Security Number=${demo.social_security_num}>
       <th>${demo.social_security_num}</th>
@@ -62,6 +157,9 @@ const displayDemos = () => {
 // select all exisiting demos (rows in database) upon website opening
 selectDemos();
 
+changebenefits();
+document.querySelector('#addemployee').addEventListener("click", function() {insertDemo(newssn)});
+
 // The following are async function to select, insert, update and delete demos
 // select all the demo
 async function selectDemos() {
@@ -71,7 +169,7 @@ async function selectDemos() {
     const jsonData = await response.json();
 
     setDemos(jsonData);
-    
+
     displayDemos();
 
   } catch (err) {
@@ -80,7 +178,6 @@ async function selectDemos() {
 }
 
 const isLetterSpace = new RegExp ('^[A-Za-z ]+$')
-const isEmail = new RegExp ('/^\S+@\S+\.\S+$/')
 
 // update a demo description
 async function updateDemo(id) {
@@ -97,28 +194,28 @@ async function updateDemo(id) {
   if(first_name == '' || last_name == '' || email == ''|| street_address == ''||
       city == '' || country == '')
   { 
-    document.querySelector('#edit-form').innerHTML += `<div class="alert alert-danger alert-dismissible">
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
                                                             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                                             <strong>Unsuccessful!</strong> Please don't leave any box empty
                                                           </div>`;
   }
 
   else if (!isLetterSpace.test(first_name) || !isLetterSpace.test(last_name)){
-    document.querySelector('#edit-form').innerHTML += `<div class="alert alert-danger alert-dismissible">
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
                                                             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                                             <strong>Unsuccessful!</strong> Please enter letters or spaces for your name.
                                                           </div>`;
   }
   
   else if (!email.includes('@') && !email.includes('.')){
-    document.querySelector('#edit-form').innerHTML += `<div class="alert alert-danger alert-dismissible">
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
                                                             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                                             <strong>Unsuccessful!</strong> Please enter a valid email address.
                                                           </div>`;
   }
 
   else if (!isLetterSpace.test(first_name) || !isLetterSpace.test(last_name)){
-    document.querySelector('#edit-form').innerHTML += `<div class="alert alert-danger alert-dismissible">
+    document.querySelector('#alert').innerHTML += `<div class="alert alert-danger alert-dismissible">
                                                             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                                             <strong>Unsuccessful!</strong> Please enter letters or spaces for your name.
                                                           </div>`;
@@ -138,7 +235,7 @@ async function updateDemo(id) {
   
       selectDemos()
       
-      document.querySelector('#edit-form').innerHTML += `<div class="alert alert-success alert-dismissible">
+      document.querySelector('#alert').innerHTML += `<div class="alert alert-success alert-dismissible">
                                                               <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                                               <strong>Success!</strong> Please close to see the changes made.
                                                             </div>`;
--- transactions.sql
+++ transactions.sql
@@ -1,45 +1,16 @@
 BEGIN;
-UPDATE employee
-SET first_name = '1', last_name = 'Laurence', email = 'alaurence9@g.co', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
+INSERT INTO employee (social_security_num, first_name, last_name, email, gender, street_num, city, country, job, current_airport_code)
+VALUES ('268-77-2527','a','b','ariyuneasaki@gmail.com','Female','2','2','2','pilot','PEK');
+INSERT INTO payment (social_security_num, job, normal_hours, overtime_hours, taxes, monthly_salary)
+VALUES ('268-77-2527', 'pilot', 0, 0, 0, 0);
 COMMIT;
+/*Insert a new employee: 695-01-5170 into employee table with the name: a a, email: ariyuneasaki@gmail.com
+    , gender: Female, street address: a, city: a, country: a, 
+    job: pilot, and current airport code: PEK. We also insert this employee into payment and set their normal hours, overtime_hours,
+    taxes and monthly salary to 0.*/
 BEGIN;
-UPDATE employee
-SET first_name = '1', last_name = 'Laurence', email = 'alaurence9@g.co', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = '1', last_name = 'Laurence', email = 'alaurence9', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = '1', last_name = 'Laurence', email = 'alaurence9', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = '1', last_name = 'Laurence', email = 'alaurence9@gmail.com', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = 'Abby', last_name = 'Laurence', email = 'alaurence9@gmail.com', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = 'Abby', last_name = 'Laurence', email = 'alaurence9@gmail.com', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = 'Abby', last_name = 'Laurence', email = 'alaurence9@gmailcom', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
-COMMIT;
-BEGIN;
-UPDATE employee
-SET first_name = 'Abby', last_name = 'Laurence', email = 'alaurence9', street_num = '5542', city = 'Sao Paulo', country = 'Brazil'
-WHERE social_security_num = '500-79-5396';
+INSERT INTO employee (social_security_num, first_name, last_name, email, gender, street_num, city, country, job, current_airport_code)
+VALUES ('695-01-5170','a','a','ariyuneasaki@gmail.com','Female','a','a','a','pilot','PEK');
+INSERT INTO payment (social_security_num, job, normal_hours, overtime_hours, taxes, monthly_salary)
+VALUES ('695-01-5170', 'pilot', 0, 0, 0, 0);
 COMMIT;
