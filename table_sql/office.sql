42cc02d127dea49eb04bbeda27dd6e11bae20b67
Tetricz <DavidLeeDaniels@protonmail.com>
2021-12-01 01:39:55 -0600

    Changed SVO to SYD on office

--- table_sql/office.sql
+++ table_sql/office.sql
@@ -4,7 +4,7 @@ create table office (
 );
 
 insert into office (office_num, airport_code) values (1, 'PEK');
-insert into office (office_num, airport_code) values (2, 'SVO');
+insert into office (office_num, airport_code) values (2, 'SYD');
 insert into office (office_num, airport_code) values (3, 'LAX');
 insert into office (office_num, airport_code) values (4, 'CDG');
 insert into office (office_num, airport_code) values (5, 'GRU');
